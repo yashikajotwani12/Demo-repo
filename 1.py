911b5d16b7dd7cf173f858632029690884a49aeb
Yashika Jotwani <yashikajotwani12@gmail.com>
2023-04-17 18:03:25 +0530

    this is new change

--- 1.py
+++ 1.py
@@ -1,6 +1,16 @@
 <p>for(int i=0;i&lt;4;i++)</p>
 
+<p>{</p>
+
+<p>&nbsp; &nbsp;for(int j=0;j&lt;i;j++)</p>
+
+<p>{</p>
+
 <p>&nbsp; &nbsp; &nbsp;print(&quot;Hello World&quot;)</p>
+
+<p>}</p>
+
+<p>}</p>
 
 <p>&nbsp;</p>
 
